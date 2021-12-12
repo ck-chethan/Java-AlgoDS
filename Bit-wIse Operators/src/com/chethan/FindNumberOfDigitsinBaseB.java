08b6186a2f50fc6c21746b893b124de6d762d04d
chethanck <chethanckm66@gmail.com>
2021-12-12 20:39:39 +0530

    Find number of digits in base b v1

--- Bit-wIse Operators/src/com/chethan/FindNumberOfDigitsinBaseB.java
+++ Bit-wIse Operators/src/com/chethan/FindNumberOfDigitsinBaseB.java
@@ -8,6 +8,16 @@ public class FindNumberOfDigitsinBaseB {
         int b = 2;
         System.out.printf("Binary digit of %d is: " + Integer.toBinaryString(n), n);
         System.out.println();
-        System.out.printf("No of Digit in %d in base %d is: " + (int)(Math.log(n)/Math.log(b))+1 ,n ,b);
+        System.out.printf("No of Digit in %d in base %d is: " + (int)((Math.log(n)/Math.log(b))+1) ,n ,b);
+        System.out.println();
+
+        //2nd Method
+        int a = n;
+        int counter = 0;
+        while(n>0){
+            n=n>>1;
+            counter++;
+        }
+        System.out.printf("No of Digit in %d in base %d is: " + counter ,a ,b);
     }
 }
