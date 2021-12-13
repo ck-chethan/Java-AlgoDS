750090cf886327785b9a9f3e5f0bbb92c4d97ff2
chethanck <chethanckm66@gmail.com>
2021-12-13 22:59:07 +0530

    Prime Number v2

--- MathsForDSA/src/com/chethan/PrimeNumbers.java
+++ MathsForDSA/src/com/chethan/PrimeNumbers.java
@@ -5,7 +5,7 @@ public class PrimeNumbers {
     public static void main(String[] args) {
         int n = 13;
         int counter = 0;
-        for (int i = 2; i < Math.floor(Math.sqrt(n)); i++) {
+        for (int i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
             if(n%i==0){counter++;}
         }
         if(counter==0) {
