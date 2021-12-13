3c1b2a5b7a24d61a39e87c1a3f94c66df8d3ed57
chethanck <chethanckm66@gmail.com>
2021-12-13 09:17:42 +0530

    Number of Set bits in a number method 2 & 3

--- Bit-wIse Operators/src/com/chethan/FindNumberOfSetBits.java
+++ Bit-wIse Operators/src/com/chethan/FindNumberOfSetBits.java
@@ -4,7 +4,13 @@ package com.chethan;
 
 public class FindNumberOfSetBits {
     public static void main(String[] args) {
-        int n = 9;
+        int n = 45;
+        method1(n);
+        method2(n);
+        method3(n);
+    }
+
+    static void method1(int n){
         int counter = 0;
         while(n>0){
             if((n&1)==1){
@@ -13,5 +19,27 @@ public class FindNumberOfSetBits {
             n = n>>1;
         }
         System.out.println(counter);
+    }
+
+    // Ex = 100101
+    // n&-n will give us Right most Set Bit.(1)
+    // n - [n&-n] -> will give 2nd set bit which is from Left hand side.
+
+    static void method2(int n){
+        int count = 0;
+        while(n>0){
+            count++;
+            n -= (n&-n);
+        }
+        System.out.println(count);
+    }
+
+    static void method3(int n){
+        int count = 0;
+        while(n>0){
+            count++;
+            n = (n&(n-1));
+        }
+        System.out.println(count);
     }
 }
