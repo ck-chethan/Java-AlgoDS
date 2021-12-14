6bb24c216f1b139449a9d54c83f9b72b5161ad5c
chethanck <chethanckm66@gmail.com>
2021-12-14 09:19:51 +0530

    Factors of a number sqrt(N) Time and Space complexity

--- MathsForDSA/src/com/chethan/FactorsOfANumber.java
+++ MathsForDSA/src/com/chethan/FactorsOfANumber.java
@@ -3,10 +3,12 @@ package com.chethan;
 // Factors
 //Ex: 20 -> 1, 2, 4, 5, 10, 20
 
+import java.util.ArrayList;
+
 public class FactorsOfANumber {
     public static void main(String[] args) {
-        int n = 20;
-        method1(n);
+        int n = 36;
+        method3(n);
     }
 
     static void method1(int n){
@@ -15,6 +17,39 @@ public class FactorsOfANumber {
             if((n % i) == 0){
                 System.out.print(i + " ");
             }
+        }
+    }
+
+    static void method2(int n){
+        // Time Complexity = O(Sqrt(N))
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if((n % i) == 0){
+                if(n/i == i){
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + " " + n / i + " ");
+                }
+            }
+        }
+    }
+
+    static void method3(int n){
+        // Both Time and Space Complexity = O(Sqrt(N))
+        // To sort the result
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if((n % i) == 0){
+                if(n/i == i){
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + " ");
+                    list.add(n/i);
+                }
+            }
+        }
+        //This will take Sqrt(N) Time
+        for (int i = list.size() - 1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
         }
     }
 }
