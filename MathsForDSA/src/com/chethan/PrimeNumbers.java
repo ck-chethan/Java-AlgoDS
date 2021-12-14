26cc112a670087de5c7a5d0806a042a02f69246e
chethanck <chethanckm66@gmail.com>
2021-12-14 07:25:40 +0530

    Prime Number v3

--- MathsForDSA/src/com/chethan/PrimeNumbers.java
+++ MathsForDSA/src/com/chethan/PrimeNumbers.java
@@ -4,15 +4,25 @@ public class PrimeNumbers {
 
     public static void main(String[] args) {
         int n = 13;
-        int counter = 0;
-        for (int i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
-            if(n%i==0){counter++;}
-        }
-        if(counter==0) {
+        boolean ans = isPrime(n);
+        if(ans) {
             System.out.printf("%d is a prime number", n);
         }
         else {
             System.out.printf("%d is not a prime number", n);
         }
+    }
+    static boolean isPrime(int n){
+        if(n <= 1) {
+            return false;
+        }
+        int c = 2;
+        while(c * c <= n){
+            if(n % c == 0) {
+                return false;
+            }
+            c++;
+        }
+        return true;
     }
 }
