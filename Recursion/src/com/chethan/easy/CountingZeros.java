9288de474f10b22e6e3b90d666742209e49b4dc8
chethanck <chethanckm66@gmail.com>
2021-12-16 07:20:45 +0530

    Counting Zeros Method 3

--- Recursion/src/com/chethan/easy/CountingZeros.java
+++ Recursion/src/com/chethan/easy/CountingZeros.java
@@ -1,10 +1,37 @@
 package com.chethan.easy;
 
 public class CountingZeros {
+
+    static int c = 0;
+    static void method2(int n){
+        if(n == 0){
+            return;
+        }
+        int rem = n % 10;
+        if(rem == 0){
+            c++;
+        }
+        method2(n/10);
+    }
+
     public static void main(String[] args) {
-        int n = 300220;
+        int n = 300302;
         int res = method1(n);
-        System.out.print(res);
+        System.out.println(res);
+        method2(n);
+        if(n==0){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(c);
+        }
+        if(n==0){
+            System.out.println(1);
+        }
+        else{
+            int res3 = method3(n);
+            System.out.println(res3);
+        }
     }
 
     static int method1(int n){
@@ -21,4 +48,21 @@ public class CountingZeros {
         }
         return count;
     }
+
+    static int method3(int n){
+        int count = 0;
+        return helper(n, count);
+    }
+
+    private static int helper(int n, int count) {
+        if(n == 0){
+            return count;
+        }
+        int rem = n % 10;
+        if(rem == 0){
+            count++;
+        }
+        return helper(n/10, count);
+    }
+
 }
