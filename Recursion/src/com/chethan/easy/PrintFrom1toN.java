8d43eafaff99dfff88a8e3de38a6f9fba02632b7
chethanck <chethanckm66@gmail.com>
2021-12-16 05:38:01 +0530

    Print From 1toN or N->1->N

---
+++ Recursion/src/com/chethan/easy/PrintFrom1toN.java
@@ -0,0 +1,17 @@
+package com.chethan.easy;
+
+public class PrintFrom1toN {
+    public static void main(String[] args) {
+        int n = 5;
+        print(n);
+    }
+
+    static void print(int n){
+        if(n == 1){
+            System.out.print(n + " ");
+            return;
+        }
+        print(n - 1);
+        System.out.print(n + " ");
+    }
+}
