9b6a07cc790d192b6bfc07faa1d37a07e7206c0f
chethanck <chethanckm66@gmail.com>
2021-12-16 06:52:34 +0530

    Reverse of a number Recursion Method 2 using helper function

--- Recursion/src/com/chethan/easy/ReverseNumber.java
+++ Recursion/src/com/chethan/easy/ReverseNumber.java
@@ -14,7 +14,7 @@ public class ReverseNumber {
     public static void main(String[] args) {
         int n = 1342;
         int res = method2(n);
-        //System.out.println(res);
+        System.out.println(res);
         method3(n);
         System.out.print(sum);
     }
@@ -30,10 +30,17 @@ public class ReverseNumber {
     }
 
     static int method2(int n){
+        // Sometimes we might need some additional variables in the arguments in that case make a function.
+        int digits = (int) (Math.log10(n)+1);
+        return helper(n, digits);
+    }
+
+    private static int helper(int n, int digits) {
         if(n % 10 == n){
             return n;
         }
-        return (10*(n % 10) + method2(n / 10));
+        int rem = n % 10;
+        return (int) (rem*Math.pow(10, digits-1) + helper(n/10, digits - 1));
     }
 
 
