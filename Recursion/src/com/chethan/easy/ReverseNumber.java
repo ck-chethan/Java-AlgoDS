34f0965e4bdbc2dc62bad80461f8f35ea2c51912
chethanck <chethanckm66@gmail.com>
2021-12-16 06:42:46 +0530

    Reverse of a number Method 3

--- Recursion/src/com/chethan/easy/ReverseNumber.java
+++ Recursion/src/com/chethan/easy/ReverseNumber.java
@@ -1,10 +1,22 @@
 package com.chethan.easy;
 
 public class ReverseNumber {
+
+    static int sum = 0;
+    static void method3(int n){
+        if(n == 0){
+            return;
+        }
+        sum = sum * 10 + (n % 10);
+        method3(n/10);
+    }
+
     public static void main(String[] args) {
-        int n = 0;
-        int res = method1(n);
-        System.out.print(res);
+        int n = 1342;
+        int res = method2(n);
+        //System.out.println(res);
+        method3(n);
+        System.out.print(sum);
     }
 
     static int method1(int n){
@@ -16,4 +28,13 @@ public class ReverseNumber {
         }
         return ans;
     }
+
+    static int method2(int n){
+        if(n % 10 == n){
+            return n;
+        }
+        return (10*(n % 10) + method2(n / 10));
+    }
+
+
 }
