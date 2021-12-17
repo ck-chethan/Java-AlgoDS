57c4e288f5636cec5c6ae2ced35e5d44ab5a0a27
chethanck <chethanckm66@gmail.com>
2021-12-17 08:28:28 +0530

    Rotated Binary Search using Recursion

---
+++ Recursion/src/com/chethan/arrays/RotatedBinarySearch.java
@@ -0,0 +1,44 @@
+package com.chethan.arrays;
+
+//  arr = {5, 6, 7, 8, 1, 2, 3};
+//  If arr[s] <= arr[mid]
+//      If key >= arr[s] and key <= arr[mid]
+//          end = mid - 1
+//     else
+//          start = mid + 1
+//  arr = {5, 6, 7, 1, 2, 3, 4};
+//  else
+//      If key >= arr[mid] and key <= arr[e]
+//          start = mid + 1
+//      else
+//          end = mid - 1
+public class RotatedBinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 7, 1, 2, 3, 4};
+        int target = 8;
+        int res = search(arr, target, 0, arr.length - 1);
+        System.out.println(res);
+    }
+
+    static int search(int[] arr, int target, int s, int e){
+        if(s>e){
+            return -1;
+        }
+        int mid = s + (e-s)/2;
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(arr[s] <= arr[mid]){
+            if(target>=arr[s] && target<=arr[mid]) {
+                return search(arr, target, s, mid - 1);
+            }
+            return search(arr, target, mid + 1, e);
+        }
+        else {
+            if(target>=arr[mid] && target<=arr[e]) {
+                return search(arr, target, mid + 1, e);
+            }
+            return search(arr, target, s, mid - 1);
+        }
+    }
+}
