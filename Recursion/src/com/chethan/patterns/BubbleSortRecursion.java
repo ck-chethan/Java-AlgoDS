51f16b0faa5c2d3d5fc97ef8e498bd018844c212
chethanck <chethanckm66@gmail.com>
2021-12-17 13:52:26 +0530

    Binary search using Recursion - Descending

--- Recursion/src/com/chethan/patterns/BubbleSortRecursion.java
+++ Recursion/src/com/chethan/patterns/BubbleSortRecursion.java
@@ -5,11 +5,14 @@ import java.util.Arrays;
 public class BubbleSortRecursion {
     public static void main(String[] args) {
         int[] arr = {4, 5, 6, 7, 3, 1, 8, 0, 2};
-        sort(arr, arr.length-1, 0);
+        sortAsc(arr, arr.length-1, 0);
         System.out.println(Arrays.toString(arr));
+        int[] arr1 = {4, 5, 6, 7, 3, 1, 8, 0, 2};
+        sortDsc(arr1, arr1.length-1, 0);
+        System.out.println(Arrays.toString(arr1));
     }
 
-    static void sort(int[] arr, int r, int c){
+    static void sortAsc(int[] arr, int r, int c){
         if(r == 0){
             return;
         }
@@ -17,12 +20,29 @@ public class BubbleSortRecursion {
             if(arr[r]<arr[c]) {
                 swap(arr, r, c);
             }
-            sort(arr, r,c + 1);
+            sortAsc(arr, r,c + 1);
         }else{
             if(arr[r]<arr[c]) {
                 swap(arr, r, c);
             }
-            sort(arr, r-1, 0);
+            sortAsc(arr, r-1, 0);
+        }
+    }
+
+    static void sortDsc(int[] arr, int r, int c){
+        if(r == 0){
+            return;
+        }
+        if(c < r){
+            if(arr[r]>arr[c]) {
+                swap(arr, r, c);
+            }
+            sortDsc(arr, r,c + 1);
+        }else{
+            if(arr[r]>arr[c]) {
+                swap(arr, r, c);
+            }
+            sortDsc(arr, r-1, 0);
         }
     }
 
