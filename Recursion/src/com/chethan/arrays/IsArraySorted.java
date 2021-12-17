585a1230ff661c80950b11c1513b4088f8846eab
chethanck <chethanckm66@gmail.com>
2021-12-17 06:26:22 +0530

    Is Array is sorted - Recursion approach

--- Recursion/src/com/chethan/arrays/IsArraySorted.java
+++ Recursion/src/com/chethan/arrays/IsArraySorted.java
@@ -2,9 +2,11 @@ package com.chethan.arrays;
 
 public class IsArraySorted {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 4, 3, 5, 7, 9};
+        int[] arr = {1, 2, 4, 3, 5, 6, 7, 8, 9};
         boolean res = isSortedLinear(arr);
+        boolean res1 = isSortedRecursion(arr);
         System.out.println(res);
+        System.out.println(res1);
     }
 
     private static boolean isSortedLinear(int[] arr) {
@@ -12,6 +14,23 @@ public class IsArraySorted {
             if(!(arr[i-1] <= arr[i])){
                 return false;
             }
+        }
+        return true;
+    }
+
+     static boolean isSortedRecursion(int[] arr) {
+        return helper(arr, 1);
+    }
+
+    static boolean helper(int[] arr, int index){
+        if(index == arr.length){
+            return true;
+        }
+        if(arr[index-1] > arr[index]) {
+            return false;
+        }
+        if(arr[index-1] < arr[index]){
+            return helper(arr, index+1);
         }
         return true;
     }
