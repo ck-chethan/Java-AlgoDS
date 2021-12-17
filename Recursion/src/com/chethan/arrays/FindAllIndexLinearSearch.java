ce4946961868974397849c4cac3ec7f279515246
chethanck <chethanckm66@gmail.com>
2021-12-17 07:02:52 +0530

    Linear Search using Recursion -- Find All Occurrences

--- Recursion/src/com/chethan/arrays/FindAllIndexLinearSearch.java
+++ Recursion/src/com/chethan/arrays/FindAllIndexLinearSearch.java
@@ -8,13 +8,13 @@ public class FindAllIndexLinearSearch {
         int[] arr = {3, 2, 3, 1, 3, 18, 3, 9};
         int target = 3;
         search(arr, 0, target);
+        System.out.println(list);
     }
 
     static ArrayList<Integer> list = new ArrayList<>();
     static void search(int[] arr, int index, int target){
 
         if(index == arr.length){
-            System.out.println(list);
             return;
         }
         if(arr[index] == target){
