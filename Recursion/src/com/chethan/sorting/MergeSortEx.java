0d97d9fb1dda5d225a23029047ac2c3b66450c35
chethanck <chethanckm66@gmail.com>
2021-12-20 23:21:23 +0530

    Merge Sort Algorithm - GFG

--- Recursion/src/com/chethan/sorting/MergeSortEx.java
+++ Recursion/src/com/chethan/sorting/MergeSortEx.java
@@ -1,34 +1,23 @@
 package com.chethan.sorting;
 
-/* Java program for Merge Sort */
 class MergeSortEx
 {
-    // Merges two subarrays of arr[].
-    // First subarray is arr[l..m]
-    // Second subarray is arr[m+1..r]
     static void merge(int[] arr, int l, int m, int r)
     {
-        // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
 
-        /* Create temp arrays */
         int[] L = new int[n1];
         int[] R = new int[n2];
 
-        /*Copy data to temp arrays*/
 //        for (int i = 0; i < n1; ++i)
 //            L[i] = arr[l + i];
         System.arraycopy(arr, l, L, 0, n1);
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
 
-        /* Merge the temp arrays */
-
-        // Initial indexes of first and second subarrays
         int i = 0, j = 0;
 
-        // Initial index of merged subarray array
         int k = l;
         while (i < n1 && j < n2) {
             if (L[i] <= R[j]) {
@@ -42,14 +31,12 @@ class MergeSortEx
             k++;
         }
 
-        /* Copy remaining elements of L[] if any */
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
 
-        /* Copy remaining elements of R[] if any */
         while (j < n2) {
             arr[k] = R[j];
             j++;
@@ -57,24 +44,18 @@ class MergeSortEx
         }
     }
 
-    // Main function that sorts arr[l..r] using
-    // merge()
     static void sort(int[] arr, int l, int r)
     {
         if (l < r) {
-            // Find the middle point
             int m =l+ (r-l)/2;
 
-            // Sort first and second halves
             sort(arr, l, m);
             sort(arr, m + 1, r);
 
-            // Merge the sorted halves
             merge(arr, l, m, r);
         }
     }
 
-    /* A utility function to print array of size n */
     static void printArray(int[] arr)
     {
         int n = arr.length;
@@ -82,7 +63,6 @@ class MergeSortEx
         System.out.println();
     }
 
-    // Driver code
     public static void main(String[] args)
     {
         int[] arr = {3, 1, 8, 7, 2, 5, 0, 4, 9, 6};
