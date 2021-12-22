629b8b8a5c66c77e79fd73f6ee31d45c4bdc67d5
chethanck <chethanckm66@gmail.com>
2021-12-22 21:36:09 +0530

    Sub sequence of a string

---
+++ Recursion/src/com/chethan/strings/SubSequence.java
@@ -0,0 +1,24 @@
+package com.chethan.strings;
+
+public class SubSequence {
+    public static void main(String[] args) {
+        String str = "abcd";
+        subseq("", str);
+    }
+
+    static void subseq(String p, String up){
+        if(up.isEmpty()){
+            if(p.isEmpty()) {
+                System.out.print(p);
+                return;
+            }
+            System.out.print(p + ", ");
+            return;
+        }
+
+        char ch = up.charAt(0);
+        subseq(p+ch, up.substring(1));
+        subseq(p, up.substring(1));
+
+    }
+}
