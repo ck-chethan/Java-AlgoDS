3dc80fead878a9ec54c9b6d2753c973eb56495ca
chethanck <chethanckm66@gmail.com>
2021-12-22 08:43:53 +0530

    Remove a specific character from a String -- Method 1 using substring and without using res in parameter

--- Recursion/src/com/chethan/strings/RemoveCharacterFromString.java
+++ Recursion/src/com/chethan/strings/RemoveCharacterFromString.java
@@ -2,11 +2,14 @@ package com.chethan.strings;
 
 public class RemoveCharacterFromString {
     public static void main(String[] args) {
-        String str = "abaccadavrrvrhhhkhdkr";
+        String str = "bacaadba";
         char tar = 'a';
         String res = removeChar(str, tar, "", 0, str.length());
         System.out.println(res);
         method1("", str, tar);
+        String out = method2(str, tar);
+        System.out.println();
+        System.out.print(out);
     }
 
     static String removeChar(String str, char tar, String ans, int s, int e) {
@@ -30,6 +33,18 @@ public class RemoveCharacterFromString {
             method1(p, up.substring(1), tar);
         } else{
             method1(p + ch, up.substring(1), tar);
+        }
+    }
+
+    static String method2(String up, char tar) {
+        if(up.isEmpty()){
+            return "";
+        }
+        char ch = up.charAt(0);
+        if(ch == tar){
+            return method2(up.substring(1), tar);
+        } else{
+            return ch + method2(up.substring(1), tar);
         }
     }
 }
