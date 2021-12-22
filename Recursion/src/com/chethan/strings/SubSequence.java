1091ab1030792152e085838e8a92bab8156a5f86
chethanck <chethanckm66@gmail.com>
2021-12-22 21:58:21 +0530

    Subsequence of a string -- using Array list

--- Recursion/src/com/chethan/strings/SubSequence.java
+++ Recursion/src/com/chethan/strings/SubSequence.java
@@ -1,9 +1,14 @@
 package com.chethan.strings;
 
+import java.util.ArrayList;
+
 public class SubSequence {
     public static void main(String[] args) {
         String str = "abc";
         subseq("", str);
+        ArrayList<String> res = subseq2("", str);
+        System.out.println();
+        System.out.println(res);
     }
 
     static void subseq(String p, String up){
@@ -20,5 +25,19 @@ public class SubSequence {
         subseq(p, up.substring(1));
         subseq(p+ch, up.substring(1));
 
+    }
+
+    static ArrayList<String> subseq2(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> left = subseq2(p, up.substring(1));
+        ArrayList<String> right = subseq2(p+ch, up.substring(1));
+        left.addAll(right);
+        return left;
     }
 }
