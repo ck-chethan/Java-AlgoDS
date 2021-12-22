ae0825ea4db3766436ed9bd89016da1431e119e6
chethanck <chethanckm66@gmail.com>
2021-12-22 09:02:32 +0530

    Remove a specific string from a String when other string not equal to some string

--- Recursion/src/com/chethan/strings/RemoveStringFromString.java
+++ Recursion/src/com/chethan/strings/RemoveStringFromString.java
@@ -6,15 +6,29 @@ public class RemoveStringFromString {
         String tar = "apple";
         String res = skip(str, tar);
         System.out.println(res);
+        String tar1 = "app";
+        String out = skipAppNotApple(str, tar1, tar);
+        System.out.println(out);
     }
     static String skip(String up, String tar) {
         if(up.isEmpty()){
             return "";
         }
         if(up.startsWith(tar)){
-            return skip(up.substring(5), tar);
+            return skip(up.substring(tar.length()), tar);
         } else{
             return up.charAt(0) + skip(up.substring(1), tar);
+        }
+    }
+
+    static String skipAppNotApple(String up, String tar, String tar1) {
+        if(up.isEmpty()){
+            return "";
+        }
+        if(up.startsWith(tar) && !up.startsWith(tar1)){
+            return skipAppNotApple(up.substring(tar.length()), tar, tar1);
+        } else{
+            return up.charAt(0) + skipAppNotApple(up.substring(1), tar, tar1);
         }
     }
 }
