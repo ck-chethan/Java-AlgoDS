be9d611cbe0f11ea07fcd2823eb7c0de82b7bd30
chethanck <chethanckm66@gmail.com>
2021-12-22 23:15:25 +0530

    Subset of a Integer

---
+++ Recursion/src/com/chethan/strings/Subset.java
@@ -0,0 +1,28 @@
+package com.chethan.strings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subset {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3};
+        List<List<Integer>> res = subset(arr);
+        System.out.println(res);
+    }
+
+    static List<List<Integer>> subset(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+
+        for (int num: arr){
+            int n = outer.size();
+            for (int i = 0; i < n; i++) {
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+
+        return outer;
+    }
+}
