138e1fd85b77f5dfda0a86d6d16ba94eca7905c2
chethanck <chethanckm66@gmail.com>
2021-12-23 23:20:53 +0530

    Permutations problem - Method 2

--- Recursion/src/com/chethan/permutation/Permutations.java
+++ Recursion/src/com/chethan/permutation/Permutations.java
@@ -1,9 +1,13 @@
 package com.chethan.permutation;
 
+import java.util.ArrayList;
+
 public class Permutations {
     public static void main(String[] args) {
         String str = "abc";
         permutation("", str);
+        ArrayList<String> res = permutationList("", str);
+        System.out.println(res);
     }
 
     static void permutation(String p, String up) {
@@ -18,5 +22,23 @@ public class Permutations {
             String s = p.substring(i, p.length());
             permutation(f + ch + s, up.substring(1));
         }
+    }
+
+    static ArrayList<String> permutationList(String p, String up) {
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+
+        for (int i = 0; i <= p.length() ; i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            ans.addAll(permutationList(f + ch + s, up.substring(1)));
+        }
+        return ans;
     }
 }
