56f449d0170720294dd20d9a9135f1ed9e159086
chethanck <chethanckm66@gmail.com>
2021-12-29 22:59:28 +0530

    Maze Problem No of ways and Paths -- ArrayList

--- Recursion/src/com/chethan/backtracking/Maze.java
+++ Recursion/src/com/chethan/backtracking/Maze.java
@@ -1,11 +1,15 @@
 package com.chethan.backtracking;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
         int n = 3;
         int res = count(n, n);
         System.out.println(res);
         path("", n, n);
+        ArrayList<String> ress = pathRet("", n, n);
+        System.out.println(ress);
     }
 
     static int count(int r, int c){
@@ -28,5 +32,21 @@ public class Maze {
         if(c > 1) {
             path(p + 'R', r, c - 1);
         }
+    }
+
+    static ArrayList<String> pathRet(String p, int r, int c){
+        if(r == 1 && c == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if(r > 1) {
+            list.addAll(pathRet(p + 'D', r - 1, c));
+        }
+        if(c > 1) {
+            list.addAll(pathRet(p + 'R', r, c - 1));
+        }
+        return list;
     }
 }
