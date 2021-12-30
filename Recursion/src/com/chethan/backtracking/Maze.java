385a5e0e01e04c4a7502ad39e16907dfe13e78f2
chethanck <chethanckm66@gmail.com>
2021-12-30 23:22:58 +0530

    Maze Problem No of ways and Paths

--- Recursion/src/com/chethan/backtracking/Maze.java
+++ Recursion/src/com/chethan/backtracking/Maze.java
@@ -6,17 +6,8 @@ public class Maze {
     public static void main(String[] args) {
         int n = 3;
         System.out.println(count(n, n));
-        System.out.println(countDiagonal(n, n));
-        System.out.println();
         path("", n, n);
-        System.out.println();
-        pathDiagonal("", n, n);
-        System.out.println();
         System.out.println(pathRet("", n, n));
-        System.out.println(pathRetDiagonal("", n, n));
-        System.out.println();
-        boolean[][] board = {{true, true, true}, {true, false, true}, {true, true, true}};
-        pathWithRestrictions("", board, 0, 0);
     }
 
     static int count(int r, int c){
@@ -55,66 +46,5 @@ public class Maze {
             list.addAll(pathRet(p + 'R', r, c - 1));
         }
         return list;
-    }
-
-    static int countDiagonal(int r, int c){
-        if(r == 1 || c == 1){
-            return 1;
-        }
-        int left = countDiagonal(r - 1, c);
-        int right = countDiagonal(r, c - 1);
-        int diagonal = countDiagonal(r - 1, c - 1);
-        return left + right + diagonal;
-    }
-
-    static void pathDiagonal(String p, int r, int c){
-        if(r == 1 && c == 1){
-            System.out.println(p);
-            return;
-        }
-        if(r > 1) {
-            pathDiagonal(p + 'V', r - 1, c);
-        }
-        if(c > 1) {
-            pathDiagonal(p + 'H', r, c - 1);
-        }
-        if(r > 1 && c > 1) {
-            pathDiagonal(p + 'D', r - 1, c - 1);
-        }
-    }
-
-    static ArrayList<String> pathRetDiagonal(String p, int r, int c){
-        if(r == 1 && c == 1){
-            ArrayList<String> list = new ArrayList<>();
-            list.add(p);
-            return list;
-        }
-        ArrayList<String> list = new ArrayList<>();
-        if(r > 1) {
-            list.addAll(pathRetDiagonal(p + 'V', r - 1, c));
-        }
-        if(c > 1) {
-            list.addAll(pathRetDiagonal(p + 'H', r, c - 1));
-        }
-        if(r > 1 && c > 1) {
-            list.addAll(pathRetDiagonal(p + 'D',r - 1,c - 1));
-        }
-        return list;
-    }
-
-    static void pathWithRestrictions(String p, boolean[][] maze, int r, int c){
-        if(r == (maze.length - 1) && c == (maze[0].length - 1)){
-            System.out.println(p);
-            return;
-        }
-        if(!maze[r][c]){
-            return;
-        }
-        if(r < maze.length - 1) {
-            pathWithRestrictions(p + 'D', maze, r + 1, c);
-        }
-        if(c < maze.length - 1) {
-            pathWithRestrictions(p + 'R', maze, r, c + 1);
-        }
     }
 }
