3e2093475c3a2623ba7411d764c75c6bae9b7f98
chethanck <chethanckm66@gmail.com>
2022-01-13 01:31:27 +0530

    Spiral Matrix - Leetcode - Medium - 54 - Method 2

--- Questions/src/com/chethan/leetCode/medium/SpiralMatrix.java
+++ Questions/src/com/chethan/leetCode/medium/SpiralMatrix.java
@@ -9,7 +9,7 @@ import java.util.List;
 class SpiralMatrix {
     public static void main(String[] args) {
         int[][] matrix = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        List<Integer> res = spiralOrder(matrix);
+        List<Integer> res = spiralOrder2(matrix);
         System.out.println(res);
     }
     static List<Integer> spiralOrder(int[][] matrix) {
@@ -44,6 +44,36 @@ class SpiralMatrix {
             }
             colBegin++;
         }
+        return res;
+    }
+
+    static List<Integer> spiralOrder2(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if(matrix.length == 0 || matrix[0].length == 0) return res;
+
+        int top = 0;
+        int bottom = matrix.length-1;
+        int left = 0;
+        int right = matrix[0].length-1;
+
+        while(true){
+            for(int i = left; i <= right; i++) res.add(matrix[top][i]);
+            top++;
+            if(left > right || top > bottom) break;
+
+            for(int i = top; i <= bottom; i++) res.add(matrix[i][right]);
+            right--;
+            if(left > right || top > bottom) break;
+
+            for(int i = right; i >= left; i--) res.add(matrix[bottom][i]);
+            bottom--;
+            if(left > right || top > bottom) break;
+
+            for(int i = bottom; i >= top; i--) res.add(matrix[i][left]);
+            left++;
+            if(left > right || top > bottom) break;
+        }
+
         return res;
     }
 }
