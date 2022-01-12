0f8d46033d24cddb2c63dc093c5f1fd946b018a0
chethanck <chethanckm66@gmail.com>
2022-01-13 01:48:04 +0530

    Spiral MatrixII - Leetcode - Medium - 59

---
+++ Questions/src/com/chethan/leetCode/medium/SpiralMatrixII.java
@@ -0,0 +1,50 @@
+package com.chethan.leetCode.medium;
+
+import java.util.Arrays;
+// https://leetcode.com/problems/spiral-matrix/
+// Leetcode - Medium - 59
+
+class SpiralMatrixII {
+    public static void main(String[] args) {
+        int n = 3;
+        int[][] res = generateMatrix(n);
+        for(int[] re: res) {
+            System.out.println(Arrays.toString(re));
+        }
+    }
+    static int[][] generateMatrix(int n) {
+        int[][] res = new int[n][n];
+        if(n==0) return res;
+        int rowBegin = 0;
+        int rowEnd = n - 1;
+        int colBegin = 0;
+        int colEnd = n - 1;
+        int count = 0;
+        while (rowBegin <= rowEnd && colBegin <= colEnd) {
+            for (int i = colBegin; i <= colEnd; i++) {
+                res[rowBegin][i] = ++count;
+            }
+            rowBegin++;
+
+            for (int i = rowBegin; i <= rowEnd; i++) {
+                res[i][colEnd] = ++count;
+            }
+            colEnd--;
+
+            if(rowBegin<=rowEnd){
+                for (int i = colEnd; i >= colBegin; i--) {
+                    res[rowEnd][i] = ++count;
+                }
+            }
+            rowEnd--;
+
+            if(colBegin<=colEnd){
+                for (int i = rowEnd; i >= rowBegin; i--) {
+                    res[i][colBegin] = ++count;
+                }
+            }
+            colBegin++;
+        }
+        return res;
+    }
+}
