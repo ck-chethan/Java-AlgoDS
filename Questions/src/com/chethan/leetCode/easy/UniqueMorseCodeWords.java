b88958105515dc15624bc0af16f6fe88590df919
chethanck <chethanckm66@gmail.com>
2022-01-13 17:12:15 +0530

    Unique Morse Code Words - Leetcode - Easy - 804

---
+++ Questions/src/com/chethan/leetCode/easy/UniqueMorseCodeWords.java
@@ -0,0 +1,33 @@
+package com.chethan.leetCode.easy;
+
+// https://leetcode.com/problems/unique-morse-code-words/
+// Leetcode - Easy - 804
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+class UniqueMorseCodeWords {
+    public static void main(String[] args) {
+        String[] words = {"gin","zen","gig","msg"};
+        int res = uniqueMorseRepresentations(words);
+        System.out.println(res);
+
+    }
+    static int uniqueMorseRepresentations(String[] words) {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        HashMap<Character, String> morseCode = new HashMap<>();
+        int count = 0;
+        for (int i = 97; i < 97 + 26; i++) {
+            morseCode.put((char) i, morse[count++]);
+        }
+        HashSet<String> res = new HashSet<>();
+        for(String word: words){
+            String out = "";
+            for (int i = 0; i < word.length(); i++) {
+                out += morseCode.get(word.charAt(i));
+            }
+            res.add(out);
+        }
+        return res.size();
+    }
+}
