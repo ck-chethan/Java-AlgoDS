435c942d5315f50f88738113d37c8748bd609278
chethanck <chethanckm66@gmail.com>
2022-01-13 16:31:32 +0530

    Unique Email Address - Leetcode - Easy - 929

---
+++ Questions/src/com/chethan/leetCode/easy/UniqueEmailAddresses.java
@@ -0,0 +1,33 @@
+package com.chethan.leetCode.easy;
+
+// https://leetcode.com/problems/unique-email-addresses/
+// Leetcode - Easy - 929
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+class UniqueEmailAddresses {
+    public static void main(String[] args) {
+        String[] emails = {"test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"};
+        int res = numUniqueEmails(emails);
+        System.out.println(res);
+    }
+    static int numUniqueEmails(String[] emails) {
+        HashSet<String> result = new HashSet<>();
+        for (String email : emails) {
+            int split_pos = email.indexOf('@');
+            String out = "";
+            for (int j = 0; j < split_pos; j++) {
+                if(Character.isLowerCase(email.charAt(j))){
+                    out += email.charAt(j);
+                }
+                if(email.charAt(j) == '+'){
+                    break;
+                }
+            }
+            out = out + email.substring(split_pos);
+            result.add(out);
+        }
+        return result.size();
+    }
+}
