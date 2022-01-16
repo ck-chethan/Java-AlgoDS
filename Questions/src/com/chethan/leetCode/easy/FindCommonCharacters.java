e6b969bf58d83905b03c1b8952b9ecf6bb46ed82
chethanck <chethanckm66@gmail.com>
2022-01-16 10:14:32 +0530

    Find Common Characters - Leetcode - Easy - 1002 -- Method 2

--- Questions/src/com/chethan/leetCode/easy/FindCommonCharacters.java
+++ Questions/src/com/chethan/leetCode/easy/FindCommonCharacters.java
@@ -10,7 +10,7 @@ import java.util.List;
 class FindCommonCharacters {
     public static void main(String[] args) {
         String[] words = {"bella","label","roller"};
-        List<String> res = commonChars(words);
+        List<String> res = commonChars2(words);
         System.out.println(res);
     }
     static List<String> commonChars(String[] words) {
@@ -33,5 +33,34 @@ class FindCommonCharacters {
             }
         }
         return commonStr;
+    }
+
+    static List<String> commonChars2(String[] words) {
+        int[] arr =new int[26];
+        for(int i=0;i<words[0].length();i++)
+        {
+            arr[words[0].charAt(i)-'a']++;
+        }
+        for(int i=1;i<words.length;i++)
+        {
+            int[] arr1=new int[26];
+            for(int j=0;j<words[i].length();j++)
+            {
+                arr1[words[i].charAt(j)-'a']++;
+            }
+            for(int j=0;j<26;j++)
+            {
+                arr[j]=Math.min(arr1[j],arr[j]);
+            }
+        }
+        List<String> ans=new ArrayList<>();
+        for(int i=0;i<26;i++)
+        {
+            for(int j=0;j<arr[i];j++)
+            {
+                ans.add(String.valueOf((char)(97+i)));
+            }
+        }
+        return ans;
     }
 }
