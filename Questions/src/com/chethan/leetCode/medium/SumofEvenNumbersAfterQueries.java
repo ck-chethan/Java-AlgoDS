3b2b9efa65c90a6dd93d1315c6a633482afe5467
chethanck <chethanckm66@gmail.com>
2022-01-22 23:46:10 +0530

    985. Sum of Even Numbers After Queries - Leetcode - Medium -- Method 2

--- Questions/src/com/chethan/leetCode/medium/SumofEvenNumbersAfterQueries.java
+++ Questions/src/com/chethan/leetCode/medium/SumofEvenNumbersAfterQueries.java
@@ -9,7 +9,7 @@ class SumofEvenNumbersAfterQueries {
     public static void main(String[] args) {
         int[] nums = {1,2,3,4};
         int[][] queries = {{1,0},{-3,1},{-4,0},{2,3}};
-        int[] res = sumEvenAfterQueries(nums, queries);
+        int[] res = sumEvenAfterQueries2(nums, queries);
         System.out.println(Arrays.toString(res));
     }
     static int[] sumEvenAfterQueries(int[] nums, int[][] queries) {
@@ -32,5 +32,24 @@ class SumofEvenNumbersAfterQueries {
             }
         }
         return sum;
+    }
+
+    static int[] sumEvenAfterQueries2(int[] A, int[][] queries) {
+        int S = 0;
+        for (int x: A)
+            if (x % 2 == 0)
+                S += x;
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; ++i) {
+            int val = queries[i][0], index = queries[i][1];
+            if (A[index] % 2 == 0) S -= A[index];
+            A[index] += val;
+            if (A[index] % 2 == 0) S += A[index];
+            ans[i] = S;
+        }
+
+        return ans;
     }
 }
