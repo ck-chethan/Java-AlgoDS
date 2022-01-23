fe0e1e56c81f92c71e0e5516d6050a0890f436ba
chethanck <chethanckm66@gmail.com>
2022-01-23 22:23:42 +0530

    459. Repeated Substring Pattern - Easy

---
+++ Questions/src/com/chethan/leetCode/easy/RepeatedSubstringPattern.java
@@ -0,0 +1,19 @@
+package com.chethan.leetCode.easy;
+
+// https://leetcode.com/problems/repeated-substring-pattern/
+// 459. Repeated Substring Pattern - Easy
+
+class RepeatedSubstringPattern {
+    public static void main(String[] args) {
+        String s = "abab";
+        boolean res = repeatedSubstringPattern(s);
+        System.out.println(res);
+    }
+    static boolean repeatedSubstringPattern(String s) {
+        boolean ans;
+        String ss =s+s;
+
+        ans=(ss.substring(1,ss.length()-1).contains(s));
+        return ans;
+    }
+}
