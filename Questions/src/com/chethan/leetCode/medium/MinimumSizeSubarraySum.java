ba4778b5f47f004638c2075a3f8041617d0951d1
chethanck <chethanckm66@gmail.com>
2022-01-26 22:16:21 +0530

    209. Minimum Size Subarray Sum - Leetcode - Medium - Method2

--- Questions/src/com/chethan/leetCode/medium/MinimumSizeSubarraySum.java
+++ Questions/src/com/chethan/leetCode/medium/MinimumSizeSubarraySum.java
@@ -7,7 +7,7 @@ class MinimumSizeSubarraySum {
     public static void main(String[] args) {
         int[] nums = {2, 3, 1, 2, 4, 3};
         int target = 7;
-        int res = minSubArrayLen(target, nums);
+        int res = minSubArrayLen2(target, nums);
         System.out.println(res);
     }
     static int minSubArrayLen(int target, int[] nums) {
@@ -23,5 +23,33 @@ class MinimumSizeSubarraySum {
             }
         }
         return ans == Integer.MAX_VALUE ? 0 : ans;
+    }
+
+    static int minSubArrayLen2(int target, int[] nums) {
+        int tracker=0;
+        int globalMin=Integer.MAX_VALUE;
+        int localSum=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>=target){
+                return 1;
+            }
+
+
+            localSum+=nums[i];
+            while(localSum>target && tracker<=i-1){
+                globalMin=Math.min(globalMin,i-tracker+1);
+                localSum-=nums[tracker];
+                tracker++;
+            }
+
+            if(localSum==target){
+                globalMin=Math.min(globalMin,i-tracker+1);
+            }
+        }
+        if(globalMin==Integer.MAX_VALUE){
+            return 0;
+        }
+
+        return globalMin;
     }
 }
