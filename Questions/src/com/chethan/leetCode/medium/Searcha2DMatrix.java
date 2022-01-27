b158215312e84b37647aa6e35a3461a5a77d7590
chethanck <chethanckm66@gmail.com>
2022-01-27 19:39:57 +0530

    74. Search a 2D Matrix - Leetcode - Medium - Method2

--- Questions/src/com/chethan/leetCode/medium/Searcha2DMatrix.java
+++ Questions/src/com/chethan/leetCode/medium/Searcha2DMatrix.java
@@ -6,8 +6,8 @@ package com.chethan.leetCode.medium;
 class Searcha2DMatrix {
     public static void main(String[] args) {
         int[][] matrix = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
-        int target = 13;
-        boolean res = searchMatrix(matrix, target);
+        int target = 3;
+        boolean res = searchMatrix2(matrix, target);
         System.out.println(res);
     }
 
@@ -28,5 +28,18 @@ class Searcha2DMatrix {
             }
         }
         return res;
+    }
+
+    static boolean searchMatrix2(int[][] matrix, int target) {
+
+        for(int[] rows : matrix){
+            if(rows[rows.length -1] >= target){
+                for(int col :rows){
+                    if(col ==target) return true;
+                }
+                return false;
+            }
+        }
+        return false;
     }
 }
