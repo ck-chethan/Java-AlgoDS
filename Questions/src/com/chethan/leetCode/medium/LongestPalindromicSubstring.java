80d7defceb5bfdfe43a74f3b841503131f4d9cfe
chethanck <chethanckm66@gmail.com>
2022-01-29 22:23:30 +0530

    5. Longest Palindromic Substring - Leetcode - Medium - Method2

--- Questions/src/com/chethan/leetCode/medium/LongestPalindromicSubstring.java
+++ Questions/src/com/chethan/leetCode/medium/LongestPalindromicSubstring.java
@@ -6,7 +6,7 @@ package com.chethan.leetCode.medium;
 class LongestPalindromicSubstring {
     public static void main(String[] args) {
         String s = "babad";
-        String res = longestPalindrome(s);
+        String res = longestPalindrome2(s);
         System.out.println(res);
     }
     static String longestPalindrome(String s) {
@@ -41,5 +41,41 @@ class LongestPalindromicSubstring {
             }
         }
         return s.substring(start, end + 1);
+    }
+
+    static String longestPalindrome2(String s) {
+        if (s == null || s.length() <= 1) {
+            return s;
+        }
+
+        String result = null;
+        int maxLen = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            int len = findPalindrome(s, i, i);
+            if (len > maxLen) {
+                maxLen = len;
+                result = s.substring(i - len / 2, i + len / 2 + 1);
+            }
+
+            len = findPalindrome(s, i, i + 1);
+            if (len > maxLen) {
+                maxLen = len;
+                result = s.substring(i - len / 2 + 1, i + len / 2 + 1);
+            }
+        }
+
+        return result;
+    }
+
+    static Integer findPalindrome(String s, Integer left, Integer right) {
+        while (left >= 0 && right < s.length()) {
+            if (s.charAt(left) != s.charAt(right)) {
+                break;
+            }
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
     }
 }
