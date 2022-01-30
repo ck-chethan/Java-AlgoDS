bf349aaa25db376c5dd8e7a2925f19f13f85c92e
chethanck <chethanckm66@gmail.com>
2022-01-30 22:38:53 +0530

    6. Zigzag Conversion - Leetcode - Medium - Method3

--- Questions/src/com/chethan/leetCode/medium/ZigzagConversion.java
+++ Questions/src/com/chethan/leetCode/medium/ZigzagConversion.java
@@ -10,7 +10,7 @@ class ZigzagConversion {
     public static void main(String[] args) {
         String s = "PAYPALISHIRING";
         int numRows = 3;
-        String res = convert2(s, numRows);
+        String res = convert3(s, numRows);
         System.out.println(res);
     }
     static String convert(String s, int numRows) {
@@ -64,6 +64,24 @@ class ZigzagConversion {
 
         StringBuilder ret = new StringBuilder();
         for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
+    }
+
+    static String convert3(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    ret.append(s.charAt(j + cycleLen - i));
+            }
+        }
         return ret.toString();
     }
 }
