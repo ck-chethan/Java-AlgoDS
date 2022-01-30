42ca0dae5381614349f8ed926c37d3112c671b05
chethanck <chethanckm66@gmail.com>
2022-01-30 22:13:37 +0530

    5. Longest Palindromic Substring - Leetcode - Medium - Method4

--- Questions/src/com/chethan/leetCode/medium/LongestPalindromicSubstring.java
+++ Questions/src/com/chethan/leetCode/medium/LongestPalindromicSubstring.java
@@ -6,7 +6,7 @@ package com.chethan.leetCode.medium;
 class LongestPalindromicSubstring {
     public static void main(String[] args) {
         String s = "babad";
-        String res = longestPalindrome3(s);
+        String res = longestPalindrome4(s);
         System.out.println(res);
     }
     static String longestPalindrome(String s) {
@@ -114,5 +114,31 @@ class LongestPalindromicSubstring {
         }
 
         return s.substring(bestStart, bestStart + maxLen);
+    }
+
+    static String longestPalindrome4(String str) {
+        int [] currentLongest = {0,1};
+        for(int i=1;i<str.length();i++)
+        {
+            int [] odd = getLongestPalindromeFrom(str, i-1,i+1);
+            int [] even = getLongestPalindromeFrom(str, i-1,i);
+
+            int [] longest = odd[1]-odd[0] > even[1]-even[0] ? odd:even;
+
+            currentLongest = currentLongest[1]-currentLongest[0] > longest[1]-longest[0]
+                    ?currentLongest: longest;
+        }
+        return str.substring(currentLongest[0], currentLongest[1]);
+    }
+
+    static int [] getLongestPalindromeFrom(String str, int leftIdx, int rightIdx){
+        while(leftIdx >=0 && rightIdx < str.length()){
+            if(str.charAt(leftIdx) != str.charAt(rightIdx)){
+                break;
+            }
+            leftIdx--;
+            rightIdx++;
+        }
+        return new int[]{leftIdx+1, rightIdx};
     }
 }
