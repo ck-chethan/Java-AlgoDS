0db9d1abbe6020d873267c12ac732a8458903ac7
chethanck <chethanckm66@gmail.com>
2022-01-30 22:32:29 +0530

    6. Zigzag Conversion - Leetcode - Medium - Method2

--- Questions/src/com/chethan/leetCode/medium/ZigzagConversion.java
+++ Questions/src/com/chethan/leetCode/medium/ZigzagConversion.java
@@ -3,11 +3,14 @@ package com.chethan.leetCode.medium;
 // https://leetcode.com/problems/zigzag-conversion/
 // 6. Zigzag Conversion - Leetcode - Medium
 
+import java.util.ArrayList;
+import java.util.List;
+
 class ZigzagConversion {
     public static void main(String[] args) {
         String s = "PAYPALISHIRING";
         int numRows = 3;
-        String res = convert(s, numRows);
+        String res = convert2(s, numRows);
         System.out.println(res);
     }
     static String convert(String s, int numRows) {
@@ -34,11 +37,33 @@ class ZigzagConversion {
                 row--;
             }
         }
-        String ans="";
+        StringBuilder ans= new StringBuilder();
         for(String str:mat){
             if(str!=null)
-                ans+=str;
+                ans.append(str);
         }
-        return ans;
+        return ans.toString();
+    }
+
+    static public String convert2(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+        for (int i = 0; i < Math.min(numRows, s.length()); i++)
+            rows.add(new StringBuilder());
+
+        int curRow = 0;
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows.get(curRow).append(c);
+            if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+            curRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder ret = new StringBuilder();
+        for (StringBuilder row : rows) ret.append(row);
+        return ret.toString();
     }
 }
