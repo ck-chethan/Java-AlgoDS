1bb37ec93248e4a8e94282b62a8875ff6eb62c0f
chethanck <chethanckm66@gmail.com>
2022-01-31 22:22:34 +0530

    14. Longest Common Prefix - Leetcode - Easy - Method2

--- Questions/src/com/chethan/leetCode/easy/LongestCommonPrefix.java
+++ Questions/src/com/chethan/leetCode/easy/LongestCommonPrefix.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 class LongestCommonPrefix {
     public static void main(String[] args) {
         String[] strs = {"flower","flow","flight"};
-        String res = longestCommonPrefix(strs);
+        String res = longestCommonPrefix2(strs);
         System.out.println(res);
     }
     static String longestCommonPrefix(String[] strs) {
@@ -21,5 +21,18 @@ class LongestCommonPrefix {
         }
         if(i>0) return strs[0].substring(0,i);
         return "";
+    }
+
+    static String longestCommonPrefix2(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String result = strs[0];
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(result)) {
+                result = result.substring(0, result.length() - 1);
+            }
+        }
+        return result;
     }
 }
