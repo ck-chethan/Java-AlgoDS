7b7054e47875a2d4a041058deaabc61c65c6759b
chethanck <chethanckm66@gmail.com>
2022-01-31 22:40:04 +0530

    1672. Richest Customer Wealth - Leetcode - Easy

---
+++ Questions/src/com/chethan/leetCode/easy/RichestCustomerWealth.java
@@ -0,0 +1,25 @@
+package com.chethan.leetCode.easy;
+
+// https://leetcode.com/problems/richest-customer-wealth/
+// 1672. Richest Customer Wealth - Leetcode - Easy
+
+class RichestCustomerWealth {
+    public static void main(String[] args) {
+        int[][] accounts = {{1, 5}, {7, 3}, {3, 5}};
+        int res = maximumWealth(accounts);
+        System.out.println(res);
+    }
+    static int maximumWealth(int[][] accounts) {
+        int max = 0;
+        for(int[] person: accounts){
+            int bal = 0;
+            for (int account: person){
+                bal+=account;
+            }
+            if(bal>max){
+                max = bal;
+            }
+        }
+        return max;
+    }
+}
