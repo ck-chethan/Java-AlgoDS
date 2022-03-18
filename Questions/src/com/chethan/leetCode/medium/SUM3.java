f5c5fe354aba19eb094e518bc73b304076ebbe03
chethanck <chethanckm66@gmail.com>
2022-03-18 23:31:26 +0530

    15. 3Sum - Leetcode - Medium

--- Questions/src/com/chethan/leetCode/medium/SUM3.java
+++ Questions/src/com/chethan/leetCode/medium/SUM3.java
@@ -6,19 +6,14 @@ package com.chethan.leetCode.medium;
 import java.util.*;
 
 class SUM3 {
-
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
         List<List<Integer>> ans = threeSum(nums);
         System.out.println(ans);
     }
-
     public static List<List<Integer>> threeSum(int[] nums) {
         HashSet<List<Integer>> ans=new HashSet<>();
-
         Arrays.sort(nums);
-
-
         for(int i=0;i<nums.length-2;i++){
             int j=i+1;
             int k=nums.length-1;
