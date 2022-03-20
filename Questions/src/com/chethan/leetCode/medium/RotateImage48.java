ca55c8bde04471fec1eaf9d4bda1fba2d3f9e73b
chethanck <chethanckm66@gmail.com>
2022-03-20 23:51:24 +0530

    48. Rotate Image - Leetcode - Medium

--- Questions/src/com/chethan/leetCode/medium/RotateImage48.java
+++ Questions/src/com/chethan/leetCode/medium/RotateImage48.java
@@ -7,7 +7,7 @@ class RotateImage48 {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
         for(int i=0; i<n; i++){
-            for(int j=i; j<n; j++){
+            for(int j=i+1; j<n; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
