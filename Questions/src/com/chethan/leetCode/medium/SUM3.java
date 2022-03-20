5a34da4adabc4b036c4bd1af53f22d62e676da32
chethanck <chethanckm66@gmail.com>
2022-03-20 23:11:34 +0530

    15. 3Sum - Leetcode - Medium - 2Pointer

--- Questions/src/com/chethan/leetCode/medium/SUM3.java
+++ Questions/src/com/chethan/leetCode/medium/SUM3.java
@@ -8,7 +8,7 @@ import java.util.*;
 class SUM3 {
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
-        List<List<Integer>> ans = threeSum(nums);
+        List<List<Integer>> ans = threeSum2(nums);
         System.out.println(ans);
     }
     public static List<List<Integer>> threeSum(int[] nums) {
@@ -31,5 +31,30 @@ class SUM3 {
             }
         }
         return new ArrayList<>(ans);
+    }
+
+    public static List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 2; i++){
+            if((i > 0 && nums[i-1] != nums[i]) || i == 0){
+                int j = i + 1;
+                int k = nums.length - 1;
+                while(j < k) {
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        output.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        while(j < k && nums[j] == nums[j + 1]) j++;
+                        j++; k--;
+                    }else if( nums[i] + nums[j] + nums[k] > 0){
+                        k--;
+                    }else
+                        j++;
+                }
+            }
+
+        }
+        return output;
     }
 }
