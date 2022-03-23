652506cb31456db7eac059b27b19437d33c607d9
chethanck <chethanckm66@gmail.com>
2022-03-23 07:56:43 +0530

    169. Majority Element - Leetcode - Easy -> Using Hashmap

--- Questions/src/com/chethan/leetCode/easy/MajorityElement169.java
+++ Questions/src/com/chethan/leetCode/easy/MajorityElement169.java
@@ -3,10 +3,13 @@ package com.chethan.leetCode.easy;
 // https://leetcode.com/problems/majority-element/
 // 169. Majority Element - Leetcode - Easy
 
+import java.util.HashMap;
+import java.util.Map;
+
 class MajorityElement169 {
     public static void main(String[] args) {
         int[] nums = {3,3,3,5,5};
-        int result = majorityElement(nums);
+        int result = majorityElement2(nums);
         System.out.println(result);
     }
     public static int majorityElement(int[] nums) {
@@ -26,5 +29,16 @@ class MajorityElement169 {
             }
         }
         return majority;
+    }
+
+    public static int majorityElement2(int[] nums){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for(Map.Entry<Integer, Integer> it: map.entrySet()){
+            if(it.getValue() > nums.length/2) return it.getKey();
+        }
+        return 0;
     }
 }
